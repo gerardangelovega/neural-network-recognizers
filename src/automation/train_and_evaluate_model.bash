b020f8a4890a6b8bbf1ae302e3c638da46965803
gerardangelovega <vegagerard1130@gmail.com>
2026-03-21 20:23:13 +0800

    IMPL: Implemented automation script to automate the training and evaluation of models

---
+++ src/automation/train_and_evaluate_model.bash
@@ -0,0 +1,18 @@
+BASE_DIR=${1-}
+LANGUAGE=${2-}
+ARCHITECTURE=${3-}
+LOSS_TERM=${4-}
+
+train_and_eval_script=recognizers/neural_networks/train_and_evaluate.bash
+
+validation_sets=(validation-short validation-long)
+trials=({1..10})
+
+for validation_data in "${validation_sets[@]}"; do
+    for trial in "${trials[@]}"; do
+        echo ""
+        echo "TRAINING AND EVALUATING: $validation_data $trial"
+        echo "COMMAND: bash $train_and_eval_script $BASE_DIR $LANGUAGE $ARCHITECTURE $LOSS_TERM $validation_data $trial"
+        bash $train_and_eval_script $BASE_DIR $LANGUAGE $ARCHITECTURE $LOSS_TERM $validation_data $trial
+    done
+done
